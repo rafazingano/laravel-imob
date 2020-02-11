07e9c862ebbf2f54b5d1c91f133bf5f34f274d3f
rafazingano <rafazingano@gmail.com>
2020-02-11 17:53:31 -0300

    initial Commit

---
+++ src/Providers/ImobServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ConfrariaWeb\Imob\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ImobServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        //$this->loadViewsFrom(__DIR__ . '/../Views', 'cwimob');
+    }
+
+    public function register()
+    {
+
+    }
+
+}
